3a9c3c9440ffcfd200c2c50161c9e12d09f8599e
Elias Couppe <elias.couppe@gmail.com>
2017-01-25 15:37:38 +0100

    MaJ plugins et AHN pour utilisation VG v5.0.8

--- src/Config.js
+++ src/Config.js
@@ -8,7 +8,7 @@ requirejs.config({
         ol          : "../lib/external/ol3/ol",
         "plugins-vg"  : "../lib/external/geoportail/plugins-vg/GpPluginVg-src",
         "plugins-ol3"  : "../lib/external/geoportail/plugins-ol3/GpPluginOl3-src",
-        vg  : "../lib/external/virtual/js/VirtualGeoWeb-5.0.7",
+        vg  : "../lib/external/virtual/js/VirtualGeoWeb-5.0.8",
         // config du logger
         "logger-cfg" : "Utils/Logger.cfg"
     }
