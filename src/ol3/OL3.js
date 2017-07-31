cfe82aca37a828073c05cdba061fa83f0549f0ba
Pierre Munier <pierre.munier@ign.fr>
2017-07-31 17:28:50 +0200

    modifs gestion des evenements layerChanged : visible, opacity, sequence

--- src/ol3/OL3.js
+++ src/ol3/OL3.js
@@ -2756,5 +2756,30 @@ define([
             source.refresh();
         };
 
+        /**
+          * Surcharge de la fonction d'IMap
+          *
+          */
+        OL3.prototype._manageLayerChangedEvent = function () {
+            // re-abonnement à l'evenement layerChanged
+            // nécessaire pour ecouter les changements de propriétés sur la nouvelle couche
+            if (this._events.hasOwnProperty("layerChanged")) {
+                var layerChangedArray = [] ;
+                // on recopie le tableau
+                this._events["layerChanged"].forEach(function (eventObj) {
+                    layerChangedArray.push(eventObj) ;
+                },
+                this) ;
+                layerChangedArray.forEach(function (eventObj) {
+                    // on oublie ...
+                    this.forget("layerChanged", eventObj.action) ;
+                    // ... pour mieux se souvenir
+                    this.listen("layerChanged", eventObj.action , eventObj.context) ;
+                },
+                this) ;
+                layerChangedArray = null ;
+            }
+        };
+
         return OL3;
     });
