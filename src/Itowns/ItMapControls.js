345adb000aa3a5097369e1e86be799b8770f6647
Elias Couppe <elias.couppe@gmail.com>
2020-01-17 12:00:34 +0100

    fix non prise en compte couche VT en 3D pour infos et legende du LS (patch5)

--- src/Itowns/ItMapControls.js
+++ src/Itowns/ItMapControls.js
@@ -97,7 +97,7 @@ ItMap.prototype.addLayerSwitcherControl = function (controlOpts) {
     for (var i = 0; i < this._layers.length; i++) {
         var layer = this._layers[i];
         // Si la couche est un MNT, on ne l'ajoute pas au layerSwitcher
-        if (layer.obj.type !== "color") {
+        if (layer.obj.type !== "color" && !layer.obj.isColorLayer) {
             continue;
         }
         this.logger.trace("[ItMap]  : layerSwitcher  : configuring layer  : " + layer.id);
